a745e0f383873b916373066b8d688b7775dbe360
syedibrahim8 <syedibrahimofficial1@gmail.com>
2025-10-22 15:14:22 +0000

    typeof Operator

---
+++ 15.js
@@ -0,0 +1,33 @@
+//typeof operator
+
+let firstname = "Ibrahim";
+let age = 20;
+let isAlive = true;
+let bankBalance = 1234567890987654n;
+let sleeping = null;
+let laptop;
+
+console.log(typeof firstname);
+console.log(typeof age);
+console.log(typeof true);
+console.log(typeof 1234567890987654n);
+console.log(typeof null);
+console.log(typeof laptop);
+
+
+let myDetails = {
+    fullName : "Syed Ibrahim",
+    aadharcard : "12345678",
+    phone : "+1234567890",
+    age : 20,
+    isAlive : true
+};
+console.log(typeof myDetails);
+
+let cfiStudents = ["Ibrahim", "Zoheb", "Rayyan"];
+console.log(typeof cfiStudents);
+
+// Note : By default all Non Primitive Datatype is object
+// In Non Primitive we have objects and arrays
+
+

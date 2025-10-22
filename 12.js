43abb17dadc8beb15bd37e991d68ad196657549b
syedibrahim8 <syedibrahimofficial1@gmail.com>
2025-10-22 15:11:25 +0000

    Bitwise Operator

---
+++ 12.js
@@ -0,0 +1,9 @@
+// Bitwise Operator
+
+let a = 10;
+let b = 2;
+console.log(a&b);             // AND operator
+console.log(a|b);             // OR operator
+console.log(a^b);             // XOR  operator
+console.log(~a);              // (~x) = -(x+1) NOT operator
+

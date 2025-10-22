0270de0deb0a00801d8683deaf072e036e2a6845
syedibrahim8 <syedibrahimofficial1@gmail.com>
2025-10-22 11:19:33 +0000

    arithmetic operators in JS

--- 10.js
+++ 10.js
@@ -25,8 +25,8 @@
  |    +    |     Addition       |  5+2   |   7   |
  |    -    |    Substraction    |  5-2   |   3   |
  |    *    |   Multiplication   |  5*2   |   10  |
- |    /    |      Division      |  5*2   |   2.5 |
- |    %    | Modulus (Remainder)|  5/2   |   1   |
+ |    /    |      Division      |  5/2   |   2.5 |
+ |    %    | Modulus (Remainder)|  5%2   |   1   |
  |    **   |    Exponential     |  2**3  |   8   |
  */
 
@@ -42,7 +42,7 @@
  console.log(a**b);
 
  // Example 2
- 
+
  let x = 5;
  let y = 6;
  c = x + y;

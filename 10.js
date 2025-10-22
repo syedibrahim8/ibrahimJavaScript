59cf345fdf0a697b2d67a94a77b9579110a96ca5
syedibrahim8 <syedibrahimofficial1@gmail.com>
2025-10-22 07:40:13 +0000

    arithmetic operations in JS

--- 10.js
+++ 10.js
@@ -28,10 +28,9 @@
  |    /    |      Division      |  5*2   |   2.5 |
  |    %    | Modulus (Remainder)|  5/2   |   1   |
  |    **   |    Exponential     |  2**3  |   8   |
-
  */
 
- // Example
+ // Example 1
 
  let a = 5;
  let b = 6;
@@ -41,3 +40,22 @@
  console.log(a/b);
  console.log(a%b);
  console.log(a**b);
+
+ // Example 2
+ 
+ let x = 5;
+ let y = 6;
+ c = x + y;
+ console.log(c);
+ d = x - y;
+ console.log(d);
+ e = x * y;
+ console.log(e);
+ f = x / y;
+ console.log(f);
+ g = x % y;
+ console.log(g);
+ h = x ** y;
+ console.log(h); 
+
+ 

ffb9cbb7dd0ec33bbb0cb8729a6817e880c1c19b
syedibrahim8 <syedibrahimofficial1@gmail.com>
2025-10-22 15:12:19 +0000

    Comparison  Operator

---
+++ 13.js
@@ -0,0 +1,17 @@
+//Comparison Operator
+
+let a = 5;       // number
+let b = "5";     // string
+let c = 3;       // number
+let d = 2;       // number
+let e = 5;       // number
+  
+console.log(a==e);
+console.log(a==b);
+console.log(a===b);
+console.log(a!=d);
+console.log(a!==b);
+console.log(a<c);
+console.log(a<=c);
+console.log(a>c);
+console.log(a>=c);

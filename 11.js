75155d16fcda146d55b9a6a2a87d38a4db115258
syedibrahim8 <syedibrahimofficial1@gmail.com>
2025-10-22 07:41:03 +0000

    assignment operations in JS

--- 11.js
+++ 11.js
@@ -1,17 +1,37 @@
 // Assignmet Operator
+/*
+ | Operator | Example  |   Same as   |
+ | -------- | -------  | ----------- |
+ |    =     |  x = 5   |  x = 5      |
+ |    +=    |  x += 5  |  x = x + 5  |
+ |    -=    |  x -= 5  |  x = x - 5  |
+ |    *=    |  x *= 5  |  x = x * 5  |
+ |    /=    |  x /= 5  |  x = x / 5  |
+ |    **=   |  x **= 5 |  x = x ** 5 |
+*/
+
+// Example 1
+ let box = 5;
+ console.log(box);
+ console.log(box+=5);
+ console.log(box-=2);
+ console.log(box*=3);
+ console.log(box/=2);
+ console.log(box**=2);
+
+// Example 2
+ let bin = 5;
+ console.log(bin);
+ bin += 5;
+ console.log(bin);
+ bin -= 2;
+ console.log(bin);
+ bin *= 3;
+ console.log(bin);
+ bin /= 2;
+ console.log(bin);
+ bin **= 2;
+ console.log(bin);
 
 
-let box = 5;
-console.log(box);
-box += 5;
-console.log(box);
-box -= 2;
-console.log(box);;
-box *= 2;
-console.log(box);
-box /= 2;
-console.log(box);
-box %= 3;
-console.log(box);
-box **= 2;
-console.log(box);
+

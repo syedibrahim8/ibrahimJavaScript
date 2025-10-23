6d1f03f7741cad264744b709e868e0ac44e1cfab
syedibrahim8 <syedibrahimofficial1@gmail.com>
2025-10-23 15:14:09 +0000

    truthy & false values

---
+++ 19.js
@@ -0,0 +1,16 @@
+// All Truthy values
+// Example : 
+/* "hello", "234", 3, 200, true, " ", [], {},
+"0", "false", 1, from -infiity to +infinity
+*/
+
+
+// All False values
+/*
+false
+0
+-0
+null
+''
+undefined
+NaN

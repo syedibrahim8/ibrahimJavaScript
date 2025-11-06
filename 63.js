62ed0d45ea2eec73614547fab0d0727add3ec3df
syedibrahim8 <syedibrahimofficial1@gmail.com>
2025-11-06 14:57:59 +0000

    practicing javascript

---
+++ 63.js
@@ -0,0 +1,10 @@
+// Ternary operator
+/*
+Its a shortcut of writing if...else statements - all in one line.
+*/
+
+// syntax => condition ? doThisIfTrue : doThisIsFalse;
+
+let a = 20;
+let output = a > 18 ? "Vote" : "No vote";
+console.log(output);
